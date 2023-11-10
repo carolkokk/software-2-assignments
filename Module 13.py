3fcdfda6f7975660b03cc632866a5a377a0abbf3
carolkokk <142995715+carolkokk@users.noreply.github.com>
2023-11-10 23:38:30 +0200

    Update Module 13.py

--- Module 13.py
+++ Module 13.py
@@ -13,10 +13,47 @@ def check_prime(number):
             primeInteger = False
 
     response = {
-        "Number": number,
-        "isPrime": primeInteger
+        "Number":number,
+        "isPrime":primeInteger
     }
     return response
 
 if __name__ == '__main__':
     app.run(use_reloader=True,host='127.0.0.1', port=5000, debug=True)
+
+
+#Exercise 13.2
+import mysql.connector
+
+from flask import Flask, redirect, url_for, request
+
+def icao_search(icao):
+    sql = "SELECT ident AS 'ICAO',name AS 'Name',municipality AS 'Location' FROM airport WHERE ident = '"+icao+"'"
+    cursor=connection.cursor(dictionary=True)
+    cursor.execute(sql)
+    result=cursor.fetchall()
+    return result
+
+connection=mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='root',
+    password='0528',
+    autocommit=True
+)
+
+app=Flask(__name__)
+@app.route('/airport/<icao>')
+def get_info_with_icao(icao):
+    result = icao_search(icao)[0]
+    response={
+        "ICAO":result["ICAO"],
+        "Name":result["Name"],
+        "location":result["Location"]
+    }
+
+    return response
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=5000)
